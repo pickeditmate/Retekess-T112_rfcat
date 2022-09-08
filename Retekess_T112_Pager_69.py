6948d7704729e91eea837c0f39336b9d21ad50ec
PickedItMate <70039993+pickeditmate@users.noreply.github.com>
2022-09-09 00:13:51 +0100

    Update Retekess_T112_Pager_69.py

--- Retekess_T112_Pager_69.py
+++ Retekess_T112_Pager_69.py
@@ -6,14 +6,14 @@
 # It turns out the retekess pagers use the EV1527 protocol, 24 bits PWM encoded
 # ie: 1110 = 1, 1000 = 0.
 #
-# bits 14 to 23 are for the pager ID (giving space for 1023 pager addresses witho only 999 being allowed.)
+# bits 14 to 23 are for the pager ID (giving space for 1023 pager addresses with only 999 being allowed.)
 # 000101011101010001000000 = PID_69
 # Pager addressing is done in reverse with bin 1 being 1000000000, 2 being 0100000000, 4 being 0010000000 ... you get the gist...
 # Restaurant addressing is bits 1 to 13 maybe???
 # 
 # I am in the process of fuzzing the protocol and will update this repo with further info, hoping for an "all pagers activate" function :)
 #
-#
+# 327us equates to 3060 baud so we will use that.
 # Flipper is preset to 
 # {CC1101_MDMCFG3, 0x32}, // Data rate is 3.79372 kBaud
 
@@ -37,7 +37,7 @@ def ConfigureD(d):
     d.setModeIDLE()
     d.setFreq(433920000)
     d.setMdmModulation(MOD_ASK_OOK)
-    d.setMdmDRate(5000)
+    d.setMdmDRate(3060)
     #d.setMaxPower()
     d.setAmpMode(1)
     d.lowball(0)
@@ -45,6 +45,10 @@ def ConfigureD(d):
 
 ConfigureD(d)
 try:
+    #d.RFxmit(b'\x88\x88\x88\x88\xee\x8e\x88\x88\x88\xe8\x88\x88\x80\x00'*10) #D020
+    #d.RFxmit(b'\x88\x88\x88\x88\xee\x8e\x88\x88\x8e\xe8\x88\x88\x80\x00'*10) #D060
+    #d.RFxmit(b'\x88\x88\x88\x88\xee\x8e\x88\x88\xe8\xe8\x88\x88\x80\x00'*10) #D0A0
+    #d.RFxmit(b'\x8e\x88\xee\xee\x8e\xee\xee\x88\x88\x8e\x88\x88\x80\x00'*10) #4F7C10
     d.RFxmit((b'\x00\xaa\x88\x8e\x8e\x8e\xee\x8e\x8e\x88\x8e\x88\x88\x88\x88'*1))
     d.setModeIDLE()
 
